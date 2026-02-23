676bff233967305d39926c91495d21fa7ed67508
katwalsanjit <sanjitkatwal@gmail.com>
2026-02-22 23:08:53 -0500

    edit done

--- todo/urls.py
+++ todo/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
   path('addTask/', views.addTask, name='addTask'),
+  path('mark_as_done/<int:pk>/', views.markAsDone, name='markAsDone'),
+  path('mark_as_undone/<int:pk>/', views.markAsUnDone, name='markAsUnDone')
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 
 
@@ -9,3 +9,19 @@ def addTask(request):
   Task.objects.create(task=task)
   return redirect('home')
   #return HttpResponse('The form is submitted')
+
+
+def markAsDone(request, pk):
+  task = get_object_or_404(Task, pk=pk)
+  task.is_completed = True
+  task.save()
+  return redirect('home')
+
+  # return HttpResponse(task)
+
+
+def markAsUnDone(request, pk):
+  task = get_object_or_404(Task, pk=pk)
+  task.is_completed = False
+  task.save()
+  return redirect('home')
